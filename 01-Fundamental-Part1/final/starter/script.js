23d625290f73907aed120cd29ee22fefb8a8e3a1
SPOTFREIGHT\jlyin <jlyin@spotinc.com>
2021-01-06 16:43:00 -0500

    third commit

--- 01-Fundamental-Part1/final/starter/script.js
+++ 01-Fundamental-Part1/final/starter/script.js
@@ -1,2 +1,31 @@
 let js = "amazing";
-js==="amazing"?alert("JavaScipt is FUN!"):alert("JavaScript is not FUN!");
+// js==="amazing"?alert("JavaScipt is FUN!"):alert("JavaScript is not FUN!");
+
+//values and Variables
+
+//assignment part 1
+
+let country = "United States";
+let continet = "North America";
+let population = 579000000;
+console.log(country,continet,population);
+
+let isIsland = false;
+let language;
+console.log(isIsland,population,country,language);
+
+//datatype
+
+//primitive data type
+
+/*
+number
+string
+boolean
+undefined -> variables not yet defined.
+null - also mean empty value
+Symbol (ES2015) - Value that is unique and cannot be changed - const name = "John"
+BigInt (ES2020) - larger integers than the Number type can hold.
+
+JavaScript has dynamic typing: We do not have to manually define the data type of 
+the value stored in a variable. Data types are determined automatically. */
