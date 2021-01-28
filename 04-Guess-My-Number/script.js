5fa70b83a362c4b761b9daa4bafe691232eeb42c
SPOTFREIGHT\jlyin <jlyin@spotinc.com>
2021-01-28 16:13:42 -0500

    guess game

--- 04-Guess-My-Number/script.js
+++ 04-Guess-My-Number/script.js
@@ -1,16 +1,66 @@
 "use strict";
 //the random to guss
 
-let randomNumber = Math.floor(Math.random() * Math.floor(20));
-console.log(randomNumber);
+let randomNumber = Math.floor(Math.random() * Math.floor(20))+1;
+
 
 //myMessage.textContent = " 🎉Correct Number!";
+let myMessage = document.querySelector(".message");
+console.log(myMessage);
 
 let inputField = document.querySelector(".guess");
-// console.log(inputField);
+let score = document.querySelector(".score");
 let evenTesting = document.querySelector(".check");
+let myScore = 20;
+let highScore = document.querySelector(".label-highscore span");
+let currentHighScore = Number(highScore.textContent);
 evenTesting.onclick = () => {
     let myGuess = inputField.value;
-    myGuess = Number(myGuess);
+        if(myScore > 0){
+            if(!myGuess){
+                myMessage.textContent = "⛔No Number!";
+            }
+            else{
     
+                myGuess = Number(myGuess);
+                if(myGuess > randomNumber){
+                    myMessage.textContent = "👎Too Big";
+                    myScore --;
+                    score.textContent = myScore;
+                }
+                else if(myGuess < randomNumber){
+                    myMessage.textContent = "👎Too Small";
+                    myScore --;
+                    score.textContent = myScore;
+                }
+                else {
+                    myMessage.textContent = "👌Correct!!"
+                    document.querySelector(".number").textContent = randomNumber;
+                    console.log(myScore,currentHighScore);
+                    myScore > currentHighScore ? highScore.textContent = myScore: null;
+                    myScore > currentHighScore ? currentHighScore = myScore: null;
+                    myScore = 0;
+                    document.querySelector("body").style.backgroundColor = "green";
+                    document.querySelector(".number").style.width = "30rem";
+                }
+            }
+        }
+        else {
+            myMessage.textContent = "🎲Game Over";
+            document.querySelector(".number").textContent = randomNumber;
+        }
+    
+    
+}
+let again = document.querySelector(".again");
+
+again.onclick = () => {
+    inputField.value = null;
+    randomNumber = Math.floor(Math.random() * Math.floor(20))+1;
+    myScore = 20;
+    myMessage.textContent = "Start guessing ..";
+    score.textContent = myScore;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
 }
