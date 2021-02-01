2937e22bc1aa17f86b82d524d619c253673b22b9
SPOTFREIGHT\jlyin <jlyin@spotinc.com>
2021-02-01 16:14:30 -0500

    finished modal project

--- 05-Modal-Window/script.js
+++ 05-Modal-Window/script.js
@@ -1,11 +1,21 @@
 "use strict";
 let hideMe = document.querySelector(".close-modal");
+let overLay = document.querySelector(".overlay");
 hideMe.onclick = () => {
-    document.querySelector(".hidden").style.display = "none";
+    document.querySelector(".modal").style.display = "none";
+    overLay.style.display = "none";
 }
 let showMe = document.querySelectorAll(".show-modal");
 showMe.forEach(data=> {
     data.onclick = ()=> {
-        document.querySelector(".hidden").style.display = "block";
+        document.querySelector(".modal").style.display = "block";
+        overLay.style.display = "block";
     }
 })
+
+document.onkeydown = (evt) => {
+    if(evt.key === "Escape" && document.querySelector(".modal").style.display == "block") {
+        document.querySelector(".modal").style.display = "none";
+        overLay.style.display = "none";
+    }
+}
